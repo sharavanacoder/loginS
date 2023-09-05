bd4af7d2ced9c55aa8d8b613308cbca6c548f5bf
sharavanacoder <138202301+sharavanacoder@users.noreply.github.com>
2023-09-05 20:30:42 +0530

    Add files via upload

---
+++ models/loginSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+let UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+let User = mongoose.model('User', UserSchema);
+module.exports = User;
